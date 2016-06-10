eebd44fbc3fb67bcceac5ac90ad5c503a3d90ac1
Irushka1982 <leshchyshyn.iryna@gmail.com>
2016-06-10 17:02:29 +0300

    changesJS

--- scripts/script.js
+++ scripts/script.js
@@ -21,21 +21,24 @@ $(document).ready(function(){
             $(".popup-content__result").hide();
             (".popup-content__result-title").empty();
         });
-/*variables to count*/
+
+  /*variables to count*/
     var priceLeft;
     var priceRight;
     var mainprice;
-/* left click function;
-get attributes;
-show-hide layers with products*/
+
+    
+/*  click function to select a product, left block; */
+  
+  
       $(".menu-old-products li").click(function(e){
           e.preventDefault();
-          var id = $(this).attr("data-productid");
+          var id = $(this).attr("data-productid");/* get attributes;*/
           $('.selected-model--left').hide();
           if ( id ) {
             $('[data-id="'+id+'"]').show();
           }
-          $(".menu-old-products").hide().css("order","2");
+          $(".menu-old-products").hide().css("order","2"); /*show-hide layers with products*/
           $(".products--left").css("order","1");
           $(".products--left").css("display","block");
           $(".popup-content__transparent-block").hide();
@@ -45,15 +48,15 @@ show-hide layers with products*/
            priceLeft = $(this).attr("data-price-left");           
        
       });
-/* right click function;
-get attributes;
-show-hide layers with products*/
+
+  /*  click function to select a product, right block; */
+ 
        $(".menu-new-products li").click(function(e){
             e.preventDefault();
-            var parent_id = $(this).attr("data-parentproductid");
+            var parent_id = $(this).attr("data-parentproductid");/* get attributes*/
             var sub_id = $(this).attr("data-subprodid");
             if ( sub_id ) { 
-            $(".menu-new-products").hide().css("order","2");
+            $(".menu-new-products").hide().css("order","2");/*show-hide layers with products and menu*/
             $(".products--right").css("order","1");
             $(".products--right").css("display","block");
             $(".selected-model--right").hide();
@@ -67,52 +70,50 @@ show-hide layers with products*/
              $(function () {
 
             if(+priceLeft < +priceRight){
-             mainprice = Math.abs(+priceLeft - +priceRight);
+             mainprice = Math.abs(+priceLeft - +priceRight);/*count*/
                       console.log('Вы заработаете' + ' ' + priceLeft + ' ' + 'грн');
                       console.log('Cумма к оплате ' + ' ' + mainprice + ' ' + 'грн');
                       $( ".popup-content__result-title" ).prepend( document.createTextNode('Вы заработаете' + ' ' + +priceLeft + ' ' + 'грн') );
                  $( ".popup-content__result-titlea" ).prepend( document.createTextNode('Cумма к оплате ' + ' ' + mainprice + ' ' + 'грн') );
-            }
+            } /*write the result to display*/
 
             else{
 
-            mainprice = Math.abs(+priceRight - +priceLeft);
+            mainprice = Math.abs(+priceRight - +priceLeft);/*count*/
                       console.log('Вы заработаете' + ' ' + priceLeft + ' ' + 'грн');
                       console.log('Cумма к оплате ' + ' ' + mainprice + ' ' + 'грн');
                  $( ".popup-content__result-title" ).prepend( document.createTextNode('Вы заработаете ' + ' ' + mainprice + ' ' + 'грн') );
-            }
+            }/*write the result to display*/
                
             });  
     }
 });
   
 /*return to the menu;
-Click on the link on the left cards-block;
-change of priorities layers
-cleans account of the results
+click on the link on the left cards-block;
 */
        $(".another-gadget--left").click(function(e){
          e.preventDefault(); 
        
           $(".popup-content__select--arrow-left").show();
-          $('.menu-old-products').show().css("order","1");
+          $('.menu-old-products').show().css("order","1");/*change of priorities layers*/
           $(".products--left").hide().css("order","2");
           $(".popup-content__transparent-block").show();
-         $(".popup-content__result-titlea").empty();
-         $(".popup-content__result-title").empty();
+
+         $(".popup-content__result-titlea").empty(); /*cleans account of the results*/
+         $(".popup-content__result-title").empty(); 
           $(".popup-content__result").hide(); 
       });
 
 /*return to the menu;
-Click on the link on the right cards-block;
-change of priorities layers
-cleans account of the results*/
+click on the link on the right cards-block;*/
+
       $(".another-gadget--right").click(function(e){
        e.preventDefault();
-          $('.menu-new-products').show().css("order","1");
+          $('.menu-new-products').show().css("order","1");/*change of priorities layers*/
           $(".products--right").hide().css("order","2"); 
           $(".popup-content__result").hide();
-          $(".popup-content__result-titlea").empty();
+          $(".popup-content__result-titlea").empty();/*cleans account of the results*/
           $(".popup-content__result-title").empty();
           $(".popup-content__select--arrow-right").show();
       });
